3ea50b3ac5cecf5a9e44defa06ed2955411c6695
Reginaldo G Santos <reginaldo.galli@hotmail.com>
2023-05-08 22:53:59 -0300

    try

--- google-bot.py
+++ google-bot.py
@@ -6,20 +6,22 @@ import time
 contador = 0
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
-while contador <= 30:
-    driver = webdriver.Chrome(options=options)
-    #driver = webdriver.Chrome()
+while contador <= 50:
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     driver.get("https://www.google.com/search?q=msa+ccb")
     time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="botstuff"]/div/div[2]/table/tbody/tr/td[3]/a/span').click()
     time.sleep(2)
     #driver.find_element("xpath", '//*[@id="botstuff"]/div/div[2]/table/tbody/tr/td[3]/a').click()
     #driver.find_element("xpath", '//*[@id="rso"]/div[8]/div/div/div[1]/div/a/div/div/span').click()
-    driver.find_element(By.PARTIAL_LINK_TEXT, 'https://msaccb.com.br').click()
-    tempo = random.randint(5,20)
-    time.sleep(tempo)
-    driver.find_element(By.XPATH, '/html/body/main/section[2]/a').click()
-    time.sleep(600)
-    driver.close()
-    contador = contador + 1
-    print(contador)
+    try:
+        driver.find_element(By.PARTIAL_LINK_TEXT, 'https://msaccb.com.br').click()
+        tempo = random.randint(5,20)
+        time.sleep(tempo)
+        driver.find_element(By.XPATH, '/html/body/main/section[2]/a').click()
+        time.sleep(600)
+    finally:
+        driver.close()
+        contador = contador + 1
+        print(contador)
