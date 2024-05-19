90f84a3c1c1a673d5bf7708a6d6e8068c07f58c6
Reginaldo G Santos <reginaldo.galli@hotmail.com>
2024-05-19 13:18:52 -0300

    lightcode-bot

--- google-bot.py
+++ google-bot.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 import random
 import time
 
@@ -11,8 +13,8 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 while contador <= 50:
     driver = webdriver.Chrome(options=options)
     #driver = webdriver.Chrome()
-    driver.get("https://www.google.com/search?q=msa+ccb")
-    print('driver.get("https://www.google.com/search?q=msa+ccb")')
+    driver.get("https://www.google.com/search?q=lightcodeapps")
+    print('driver.get("https://www.google.com/search?q=lightcodeapps")')
     time.sleep(2)
     #driver.find_element(By.XPATH, '//*[@id="botstuff"]/div/div[2]/table/tbody/tr/td[3]/a/span').click()
     #print('driver.find_element(By.XPATH, //*[@id="botstuff"]/div/div[2]/table/tbody/tr/td[3]/a/span).click()')
@@ -20,13 +22,19 @@ while contador <= 50:
     #driver.find_element("xpath", '//*[@id="botstuff"]/div/div[2]/table/tbody/tr/td[3]/a').click()
     #driver.find_element("xpath", '//*[@id="rso"]/div[8]/div/div/div[1]/div/a/div/div/span').click()
     try:
-        driver.find_element(By.PARTIAL_LINK_TEXT, 'https://msaccb.com.br').click()
+        driver.find_element(By.PARTIAL_LINK_TEXT, 'Lightcode Apps').click()
         print('driver.find_element(By.PARTIAL_LINK_TEXT, https://msaccb.com.br).click()')
         tempo = random.randint(5,20)
         time.sleep(tempo)
-        driver.find_element(By.XPATH, '/html/body/main/section[2]/a').click()
+        driver.find_element(By.XPATH, '/html/body/header/nav/div/ul/div/li[2]/a').click()
         print('driver.find_element(By.XPATH, /html/body/main/section[2]/a).click()')
-        time.sleep(600)
+        time.sleep(5)
+        iframe = driver.find_element(By.XPATH, "/html/body/main/section[5]")
+        ActionChains(driver)\
+            .scroll_to_element(iframe)\
+            .perform()
+
+        time.sleep(180)
     finally:
         driver.close()
         contador = contador + 1
